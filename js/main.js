f20e73462689e3fc9135df0a5fb0bc43bc112435
noemi23 <noemiconde073@gmail.com>
2026-04-02 22:37:44 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+// Menú hamburguesa
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('header nav');
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
